e04a30c519f4658a072f7a444308f48e83e60ae7
Jon Mackey <jon@jonmackey.party>
2018-11-11 20:58:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,6 @@ def badge_maker(attendees)
   return "Hello, my name is #{attendees}."
 end
 
-def badge_creator(attendees)
+def batch_badge_creator(attendees)
   attendees.collect {|x| puts "Hello, my name is #{x}."}
 end
