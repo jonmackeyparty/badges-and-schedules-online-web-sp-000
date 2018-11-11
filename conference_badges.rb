d041adf8e38be11257f8370a708741a36bc6c35d
Jon Mackey <jon@jonmackey.party>
2018-11-11 21:12:31 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,8 @@ end
 def assign_rooms(attendees)
   room = 1
   room_assignments = []
-  attendees.each {|x| room_assignments << "Hello, #{x}!  You'll be assigned to room #{room}", room += 1}
+  attendees.each {|x| room_assignments << "Hello, #{x}!  You'll be assigned to room #{room}"}
+  room += 1
   return room_assignments
 end
   
